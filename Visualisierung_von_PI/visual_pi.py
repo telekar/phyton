cce9c3832a7bce28f2b124d4045cb624346755e2
telekar <fipostbox@gmx.de>
2024-10-27 14:26:13 +0100

    finish

--- Visualisierung_von_PI/visual_pi.py
+++ Visualisierung_von_PI/visual_pi.py
@@ -1,7 +1,24 @@
-with open ( "OneMill_Pi.txt","r" ) as f:
+import turtle as tu
+
+lines = 100_000
+
+with open ( "One_Mill_Pi.txt","r" ) as f:
 
     pi = f.read()
 
-print (len(pi))
-print(pi[0:10])
-print(pi[-1])
+tu.mode("logo")
+tu.tracer(False)
+tu.screensize(6000,6000,"black")
+#tu.pencolor("red")
+tu.colormode(255)
+
+for n in range(lines):
+    color = int(n/(lines/255))
+    tu.pencolor(255,255-color,color)
+    number = int(pi[n])
+    rotation = number * 36
+    tu.setheading(rotation)
+    tu.forward(3)
+    if n % 10_000 == 0:
+        tu.update()
+tu.done()
