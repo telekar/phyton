217182c5031caa3cc8f3211820486cc76e710eb4
telekar <fipostbox@gmx.de>
2024-11-26 17:05:48 +0100

    .

--- AoC/2015/test.py
+++ AoC/2015/test.py
@@ -1,43 +1,9 @@
-with open ( "test.txt","r") as f:
-    inputstr = f.read()
+distances = {}
 
-for n in range(len(inputstr)):
-    inputList = inputstr.split("\n")
-
-print(inputList)
-
-def checkLine(line):
-    copy = line
-    newLine = ""
-    count = 0
-    k=0
-    line = line.replace('\\\\',"????")
-    line = line.replace('\\x',"???")
-    line = line.replace('\\"',"????")
-    
-    print(line)
-    """for i in range(len(copy)+k):
-        print("start:",len(copy))
-        esc = (copy[i:i+2])
-        if esc == '\\x':
-            copy = copy[:i] + "???" + copy[i+2:]
-            k += 1
-            print(len(copy))
-        elif esc == '\\"':
-            copy = copy[:i] + "???" + copy[i+2:]
-            print(len(copy))
-        elif esc == '\\\\':
-            copy = copy[:i] + "????" + copy[i+2:]
-            k += 2
-            print(len(copy))
-            print(copy)
-    """  
-            
-            
-    return (line)
-        
-
-for line in inputList:
-    nline = checkLine(line)
-    print(nline)
-    print (4+len(nline))
+with open ('test.txt', 'r') as f:
+    for line in f:
+        cities, distance = line.split(' = ')
+        city1, city2 =cities.split(' to ')
+        distances[(city1, city2)] = int(distance)
+        print (city1,city2,distance)
+print(distances)
