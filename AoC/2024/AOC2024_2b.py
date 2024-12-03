e58ffdde0aa67d2109c955d2b08b49027ecfadcd
telekar <fipostbox@gmx.de>
2024-12-03 11:10:37 +0100

    update

--- AoC/2024/AOC2024_2b.py
+++ AoC/2024/AOC2024_2b.py
@@ -1,47 +1,46 @@
 with open ( "puzzle_2a","r") as f:
     input = f.read()
 
-levels = ()
+reports = ()
 
 countSave = 0
 countSave2 = 0
 
 for n in range(len(input)):
-    levels = input.split("\n")
+    reports = input.split("\n")
 
 def checkLines(numbers):
     
-    orientation = int(numbers[1]) - int(numbers[0])
-    
-    if orientation == 0 or abs(orientation) >3:
+    orientation = int(numbers[1]) - int(numbers[0]) #checking orientation of numbers
+     
+    if orientation == 0 or abs(orientation) >3: # checking differenc
         return False
         
     for i in range(1, len(numbers) - 1):
-        diff = int(numbers[i+1]) - int(numbers[i])
+        diff = int(numbers[i+1]) - int(numbers[i])  #set orientation
         if diff * orientation <= 0:
-            return False
+            return False    #orientation has changed
 
-        if abs(diff) == 0 or abs(diff) > 3:
+        if abs(diff) == 0 or abs(diff) > 3: # checking differenc
             return False
     return True
         
 def checkLines2(numbers):
     orientation = int(numbers[1]) - int(numbers[0])
-    print(numbers)
+    #print(numbers)
     
-    if orientation == 0 or abs(orientation) >3:
-        numbers = numbers[:1] + numbers[2:]
-        print(numbers)
-        if orientation == 0 or abs(orientation) >3:
-            return (numbers)
+    if orientation == 0 or abs(orientation) >3: # checking differenc at beginning
+        numbers = numbers[:1] + numbers[2:]     # delete wrong number
+        #print(numbers)
+        return (numbers)
       
         
     for i in range(1, len(numbers) - 1):
         diff = int(numbers[i+1]) - int(numbers[i])
 
-        if diff * orientation <= 0:
-            numbers = numbers[:i+1] + numbers[i+2:]
-            print(numbers)
+        if diff * orientation <= 0: #orientation has changed
+            numbers = numbers[:i+1] + numbers[i+2:] # delete wrong number
+            #print(numbers)
             return (numbers)
 
         if abs(diff) == 0 or abs(diff) > 3:
@@ -50,17 +49,17 @@ def checkLines2(numbers):
             return (numbers)
     return (numbers)
 
-for level in levels:
+for level in reports:
     level = level.split()
     
     if checkLines(level):
         countSave += 1
 
     numbers = checkLines2(level)
-    if numbers == level or checkLines(level):
+    if numbers == level or checkLines(level):   # no change in numbers == good
         countSave2 += 1
-    else:
-        if numbers == checkLines2(numbers):
+    else:                                       # first change 
+        if numbers == checkLines2(numbers):     # only one change == good
             countSave2 += 1
 
 print("Part 1:",countSave)
