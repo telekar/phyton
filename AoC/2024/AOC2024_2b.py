20e04115766de08531c677ce35507febe7325511
telekar <fipostbox@gmx.de>
2024-12-10 01:56:23 +0100

    update

--- AoC/2024/AOC2024_2b.py
+++ AoC/2024/AOC2024_2b.py
@@ -8,6 +8,7 @@ countSave2 = 0
 
 for n in range(len(input)):
     reports = input.split("\n")
+print('reports',reports)
 
 def checkLines(numbers):
     
@@ -27,11 +28,11 @@ def checkLines(numbers):
         
 def checkLines2(numbers):
     orientation = int(numbers[1]) - int(numbers[0])
-    #print(numbers)
+    print("check2:",numbers)
     
     if orientation == 0 or abs(orientation) >3: # checking differenc at beginning
         numbers = numbers[:1] + numbers[2:]     # delete wrong number
-        #print(numbers)
+        print("2 start orientation too big",numbers)
         return (numbers)
       
         
@@ -40,27 +41,34 @@ def checkLines2(numbers):
 
         if diff * orientation <= 0: #orientation has changed
             numbers = numbers[:i+1] + numbers[i+2:] # delete wrong number
-            #print(numbers)
+            print("2: orientation change",numbers)
             return (numbers)
 
         if abs(diff) == 0 or abs(diff) > 3:
+            
             numbers = numbers[:i+1] + numbers[i+2:]
-            print(numbers)
+            print("2: diff big",numbers)
             return (numbers)
     return (numbers)
 
 for level in reports:
     level = level.split()
-    
     if checkLines(level):
         countSave += 1
 
+for level in reports:
+    level = level.split()
     numbers = checkLines2(level)
-    if numbers == level or checkLines(level):   # no change in numbers == good
+    if numbers == level:   # no change in numbers == good
         countSave2 += 1
+        print(countSave2-1,countSave2)
     else:                                       # first change 
         if numbers == checkLines2(numbers):     # only one change == good
             countSave2 += 1
+            print(countSave2-1,countSave2)
+        else:
+            print("2xbad")
+    print()
 
 print("Part 1:",countSave)
 print("Part 2:",countSave2)
